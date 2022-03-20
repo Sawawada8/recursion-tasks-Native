99da595a9f7567cd0e42ffebcd8a08d89618c4cc
sawada <sawada@csplan.jp>
2022-03-20 23:25:42 +0900

    traial UI lib "NativeBase"

--- components/Example.js
+++ components/Example.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, {
+  useState
+} from 'react';
 
 import {
   View,
-  Text
+  Text,
+  Icon
 } from 'react-native';
+
+import { Entypo } from "@native-base/icons";
 
 
 import {  Box,Button } from "native-base";
 
 const Example = () => {
+  const [i, setI] = useState(0);
+
   return (
       <View>
         <Text>
           MY COMPONENT comfsd;
         </Text>
         <Box>Hello world</Box>
-        <Button>on Pressssss</Button>
+        <Button onPress={() => setI(i + 1)}>on Pressssss{i}</Button>
+{/* <Icon as={Entypo} name="user"></Icon> */}
       </View>
   );
 };
