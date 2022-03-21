d6e478e0ac1f673ac17f25f252293937fea3a928
sawada <sawada@csplan.jp>
2022-03-21 21:05:15 +0900

    wip

---
+++ MEMO.md
@@ -0,0 +1,12 @@
+- icon => SVG， Path で作成していく
+```
+<Icon>
+    <Svg height="100" width="100">
+        <Path
+        d="M25 10 L98 65 L70 25 L16 77 L11 30 L0 4 L90 50 L50 10 L11 22 L77 95 L20 25"
+        fill="none"
+        stroke="red"
+        />
+    </Svg>
+</Icon>
+```
--- src/components/EX.js
+++ src/components/EX.js
@@ -8,6 +8,7 @@ import Animated, {
   useAnimatedStyle,
   withSpring,
 } from 'react-native-reanimated';
+import {Svg, Path} from 'react-native-svg'
 
 export default () => {
   const offset = useSharedValue(0);
@@ -22,6 +23,15 @@ export default () => {
     <>
       <Animated.View style={[styles.box, animatedStyles]} />
       <Icon name="home" />
+      <Icon>
+      <Svg height="100" width="100">
+      <Path
+        d="M25 10 L98 65 L70 25 L16 77 L11 30 L0 4 L90 50 L50 10 L11 22 L77 95 L20 25"
+        fill="none"
+        stroke="red"
+      />
+    </Svg>
+    </Icon>
       <Button
         onPress={() => {
           offset.value = withSpring(Math.random());
