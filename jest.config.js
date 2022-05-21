6540914d31a0d9ae8e27a232105af79c2ad3c56b
sawada <sawada@csplan.jp>
2022-05-21 09:59:45 +0900

    jest mock setup

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+  },
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)'],
+  setupFiles: ['<rootDir>/jest.setup.js'],
 };
